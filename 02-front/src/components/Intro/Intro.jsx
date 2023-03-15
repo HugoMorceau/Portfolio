f9b2cf396e86036f627a6b02759b1d3676bdbf4e
hugo morceau <27mhugo@gmail.com>
2023-03-15 09:55:33 +0100

    feat: animate download cv button

--- 02-front/src/components/Intro/Intro.jsx
+++ 02-front/src/components/Intro/Intro.jsx
@@ -7,16 +7,22 @@ const profil = require('../../assets/images/profil.jpg')
 
 export default function Intro () {
   const hellos = ['Hello there, I am', 'Hola, soy', 'Bonjour, je suis']
-  const [hello, setHello] = useState(hellos[0])
-  const [langIndex, setLangIndex] = useState(0)
   const { t } = useTranslation()
 
+  // States
+  const [hello, setHello] = useState(hellos[0])
+  const [langIndex, setLangIndex] = useState(0)
+  const [flashReverse, setFlashState] = useState(true)
+
+  // Hooks
+  // Animate and changes lang of the hello message every 2.5s
   useEffect(() => {
     const interval = setInterval(() => {
       const nextLangIndex = langIndex === hellos.length - 1 ? 0 : langIndex + 1
       animateIntro(nextLangIndex)
       setLangIndex(nextLangIndex)
     }, 2500)
+
     return () => clearInterval(interval)
   }, [langIndex])
 
@@ -41,6 +47,14 @@ export default function Intro () {
     }, 30)
   }
 
+  const handleMouseEnter = () => {
+    setFlashState(false)
+  }
+
+  const handleMouseLeave = () => {
+    setFlashState(true)
+  }
+
   return (
     <div className='Intro'>
       <div className='titles' >
@@ -58,11 +72,24 @@ export default function Intro () {
       </div>
       <p>{t('Intro')}</p>
       <div className='buttons-container'>
+        {/*
+        <button className='button'>
+          <a href={`${process.env.PUBLIC_URL}/resume/CV-ESP.pdf`} download="CV-ESP.pdf">Download Resume</a>
+        </button> */}
         <button className='button2'>
           <a href={`${process.env.PUBLIC_URL}/resume/CV-ESP.pdf`} target="_blank" rel="noopener noreferrer">View Resume</a>
         </button>
-        <button className='button'>
-          <a href={`${process.env.PUBLIC_URL}/resume/CV-ESP.pdf`} download="CV-ESP.pdf">Download Resume</a>
+        <button
+          className={`button ${flashReverse ? 'button-flash-reverse' : ''}`}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <a
+            href={`${process.env.PUBLIC_URL}/resume/CV-ESP.pdf`}
+            download="CV-ESP.pdf"
+          >
+            Download Resume
+          </a>
         </button>
       </div>
     </div>
