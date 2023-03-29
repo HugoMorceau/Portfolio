32181d51979355bb0ae62bc268144d0c2e33fc3e
hugo morceau <27mhugo@gmail.com>
2023-03-29 10:00:45 +0200

    sidebar : rework css

--- 02-front/components/SideBar/SideBar.jsx
+++ 02-front/components/SideBar/SideBar.jsx
@@ -1,6 +1,6 @@
-import './SideBar.module.scss'
-import { FiGithub, FiLinkedin, FiTwitter, FiMail } from 'react-icons/fi'
-import { useEffect, useState } from 'react'
+import styles from './SideBar.module.scss';
+import { FiGithub, FiLinkedin, FiTwitter, FiMail } from 'react-icons/fi';
+import { useEffect, useState } from 'react';
 
 const Sidebar = () => {
   const socialMedia = [
@@ -8,21 +8,21 @@ const Sidebar = () => {
     { icon: FiLinkedin, label: 'LinkedIn', link: 'https://www.linkedin.com/in/hugo-morceau-5b36051b7/' },
     { icon: FiTwitter, label: 'Twitter', link: 'https://twitter.com/MorceauHugo' },
     { icon: FiMail, label: 'Email', link: 'mailto:hugo.morceau@gmail.com' }
-  ]
-  const [isActive, setIsActive] = useState(false)
+  ];
+  const [isActive, setIsActive] = useState(false);
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsActive(true)
-    }, 2000)
+      setIsActive(true);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
 
-    return () => clearTimeout(timer)
-  }, [])
   return (
-    <div className={`sidebar ${isActive ? 'active' : ''}`}>
+    <div className={`${styles.sidebar} ${isActive ? styles.active : ''}`}>
       <ul>
         {socialMedia.map((media) => (
-          <li key={media.label} className= "icon">
-            <div className="icon-wrapper">
+          <li key={media.label} className={styles.icon}>
+            <div className={styles.iconWrapper}>
               <a
                 key={media.label}
                 href={media.link}
@@ -30,14 +30,14 @@ const Sidebar = () => {
                 rel="noopener noreferrer"
                 title={media.label}
               >
-                <media.icon size="1.5rem"/>
+                <media.icon size="1.5rem" />
               </a>
             </div>
           </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
