cf641cee085adee58b04e6d1c4ec528ad8ba0f7d
Hugo Morceau <106115253+HugoMorceau@users.noreply.github.com>
2024-04-14 19:13:01 +0200

    update xp

--- 02-front/components/Skills/Skills.jsx
+++ 02-front/components/Skills/Skills.jsx
@@ -1,10 +1,27 @@
-import skills from '../../data/skills/skillsFR.js';
-import styles from './Skills.module.scss';
 import {
-  FaHtml5, FaCss3, FaNodeJs, FaReact, FaDatabase, FaGithub, FaVuejs, FaDocker,
-  SiIbm, SiNextdotjs, SiMongodb, SiTypescript, SiGraphql, SiThreedotjs,
-  IoCodeSlash, TbRelationOneToMany,
-} from '../../config/icons/icons.js';
+  FaCss3,
+  FaDatabase,
+  FaDocker,
+  FaGithub,
+  FaHtml5,
+  FaNodeJs,
+  FaReact,
+  FaVuejs,
+  IoCodeSlash,
+  SiDatocms,
+  SiGatsby,
+  SiGraphql,
+  SiIbm,
+  SiMongodb,
+  SiNestjs,
+  SiNextdotjs,
+  SiStrapi,
+  SiThreedotjs,
+  SiTypescript,
+  TbRelationOneToMany,
+} from "../../config/icons/icons.js";
+import skills from "../../data/skills/skillsFR.js";
+import styles from "./Skills.module.scss";
 
 const iconMap = {
   fahtml5: FaHtml5,
@@ -22,31 +39,43 @@ const iconMap = {
   favuejs: FaVuejs,
   fadocker: FaDocker,
   sigraphql: SiGraphql,
-  sithreedotjs: SiThreedotjs
-
+  sithreedotjs: SiThreedotjs,
+  sinestjs: SiNestjs,
+  sigatsby: SiGatsby,
+  sidatocms: SiDatocms,
+  sistrapi: SiStrapi,
 };
 
 export default function Skills() {
   // Todo translate categories
   const categories = {
-    web: { label: 'Web', skills: [] },
-    db: { label: 'Database', skills: [] },
-    old: { label: 'Legacy Systems', skills: [] },
-    learning: { label: 'Currently learning', skills: [] },
-    learningNext: { label: 'Want to learn', skills: [] }
+    webf: { label: "Front", skills: [] },
+    webb: { label: "Back", skills: [] },
+    db: { label: "Data", skills: [] },
+    cms: { label: "CMS", skills: [] },
+    other: { label: "Other", skills: [] },
+    old: { label: "Legacy Systems", skills: [] },
+    learning: { label: "Currently learning", skills: [] },
+    learningNext: { label: "Want to learn", skills: [] },
   };
 
   skills.forEach((skill) => {
     if (categories[skill.category]) {
       categories[skill.category].skills.push(skill);
+    } else {
+      categories.other.skills.push(skill);
     }
   });
 
   return (
     <div className={styles.SkillsContainer}>
       {Object.values(categories).map((category) => (
-        <div key={category.label}
-          className={`${styles.SkillsCategory} ${styles[`SkillsCategory--${category.label.toLowerCase()}`]}`}>
+        <div
+          key={category.label}
+          className={`${styles.SkillsCategory} ${
+            styles[`SkillsCategory--${category.label.toLowerCase()}`]
+          }`}
+        >
           <h2>{category.label}</h2>
           {category.skills.map((skill) => {
             const iconName = skill.icon.toLowerCase();
